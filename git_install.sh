2173572e571724fb698fd063583b84ced337167b
Ian Christensen <ian@datacombusinesssystems.com>
2021-09-08 13:04:00 -0700

    git-install

--- git_install.sh
+++ git_install.sh
@@ -1,6 +1,6 @@
+source ~/secrets.sh
+
 ssh-keygen -t ed25519 -y
-
-
 
 ## Add a SSH-Key (type "user:passwd ^D"), output: JSON object, or JSON error
 curl -X POST -u {{ GITHUB_USER_ID }}:{{ GITHUB_USER_PW }} https://api.github.com/user/keys --data "{\"title\": \"{{ GITHUB_KEY_TITLE }}\", \"key\": \"$(cat $HOME/.ssh/id_ed25519.pub)\"}"
